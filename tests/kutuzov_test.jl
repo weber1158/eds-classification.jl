1f9977f37f012ea6e2e8df815d2aea3c014c8a83
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2026-08-13 14:53:04 -0400

    Created `tests` folder

---
+++ tests/kutuzov_test.jl
@@ -0,0 +1,20 @@
+## Import packages / functions
+using CategoricalArrays, CSV, DataFrames
+include("../algorithms/kutuzov_classification.jl");
+
+
+## Load example data 
+atp = CSV.read("./data/eds_atom_percents.csv", DataFrame); 
+
+
+## Print first row to visualize the structure of the data 
+display(atp[2,:])
+
+
+## Apply Donarummo classification algorithm the full table 
+kut = kutuzov_classification(atp);
+
+
+## Display result for an observation (i.e., a row) in the input data 
+observation = 2
+println("Kutuzov ID #", observation, ": ", kut[observation, 1])
