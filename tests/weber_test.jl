770cc15bad77e09b05a585a2f5c1ec161324a423
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2026-08-13 15:02:15 -0400

    Update `weber_test.jl`
    Fixed typo on Line 10.

--- tests/weber_test.jl
+++ tests/weber_test.jl
@@ -7,7 +7,7 @@ include("../algorithms/weber_classification.jl");
 net = CSV.read("./data/eds_net_intensities.csv", DataFrame); 
 
 
-## Print first row to visualize the structure of the data 
+## Print second row to visualize the structure of the data 
 display(net[2,:])
 
 
