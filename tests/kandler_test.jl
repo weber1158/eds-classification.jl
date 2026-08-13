ddd5440cc8677e5ad5f654b9a0bfa293d6508fe6
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2026-08-13 15:00:49 -0400

    Update `kandler_test.jl`
    Removed `CategoricalArrays` package, which I don't think is needed. Also fixed a typo in Line 10.

--- tests/kandler_test.jl
+++ tests/kandler_test.jl
@@ -1,5 +1,5 @@
 ## Import packages / functions
-using CategoricalArrays, CSV, DataFrames
+using CSV, DataFrames
 include("../algorithms/kandler_classification.jl");
 
 
@@ -7,7 +7,7 @@ include("../algorithms/kandler_classification.jl");
 atp = CSV.read("./data/eds_atom_percents.csv", DataFrame); 
 
 
-## Print first row to visualize the structure of the data 
+## Print the second row to visualize the structure of the data 
 display(atp[2,:])
 
 
